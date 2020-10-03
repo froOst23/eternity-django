39eb45a912ad6975848fe10b1d3befc28030a80b
froOst23 <frost.dat@gmail.com>
2020-10-03 22:39:21 +0500

    change STATICFILES_DIRS

--- eternity/eternity/settings.py
+++ eternity/eternity/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "main/static"),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static/'
+]
