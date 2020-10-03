2204b5978df53863668a3f9e1c264726a69a2469
froOst23 <frost.dat@gmail.com>
2020-10-03 19:34:09 +0500

    add STATICFILES_DIRS

--- eternity/eternity/settings.py
+++ eternity/eternity/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static"),
+)
