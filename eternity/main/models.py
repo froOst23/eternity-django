0cacfe9546e0c0be3f04a3b763efd8509aad0f87
froOst23 <frost.dat@gmail.com>
2020-10-13 15:15:15 +0500

    increase content field lenght (model Comment)

--- eternity/main/models.py
+++ eternity/main/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(verbose_name='Содержание', max_length=100)
+    content = models.TextField(verbose_name='Содержание', max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
