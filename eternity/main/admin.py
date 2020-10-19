664fd9ae1d5b37b20781eacced36878d9413d75d
froOst23 <frost.dat@gmail.com>
2020-10-19 16:04:19 +0500

    changed visual design, add filter, search field

--- eternity/main/admin.py
+++ eternity/main/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Post, Profile, Comment
 
-admin.site.register(Post)
-admin.site.register(Profile)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'date')
+    search_fields = ('id', 'date', 'title', 'content')
+    list_filter = ('id', 'author', 'date')
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created', 'is_online')
+    search_fields = ('id', 'user', 'created')
+    list_filter = ('id', 'user', 'created', 'is_online')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'updated')
+    search_fields = ('id', 'author', 'post')
+    list_filter = ('id', 'author', 'post', 'updated')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Comment, CommentAdmin)
