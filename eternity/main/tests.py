a3f46f080476b41e7580a9cbcb8695ebf5f99bf5
froOst23 <frost.dat@gmail.com>
2020-11-09 18:04:26 +0500

    add simple test for testing users in project

--- eternity/main/tests.py
+++ eternity/main/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from main.models import User, Profile, Post
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class CreateUserTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        # создаем тестового пользователя - 1
+        testUser1 = User.objects.create(is_superuser=False,
+                            password='p@ssw0rd',
+                            username='testUser1',
+                            email='testUser@gmail.com',
+                            first_name='userFirstName',
+                            last_name='userLastName')
+        # создаем тестового пользователя - 2
+        testUser2 = User.objects.create(is_superuser=False,
+                            password='p@ssw0rd',
+                            username='testUser2',
+                            email='testUser@gmail.com',
+                            first_name='userFirstName',
+                            last_name='userLastName')
+
+        testPost = Post.objects.create(author=testUser1, title='testPostTitle', tag='testTag', content='testContent')
+
+
+    def testUserParam(self):
+        # выполняем проверку тестового пользователя - 1
+        testUser1 = User.objects.get(username='testUser1')
+        self.assertEquals(testUser1.username, 'testUser1')
+        self.assertEquals(testUser1.password, 'p@ssw0rd')
+        self.assertEquals(testUser1.email, 'testUser@gmail.com')
+        self.assertEquals(testUser1.first_name, 'userFirstName')
+        self.assertEquals(testUser1.last_name, 'userLastName')
+        # выполняем проверку расширенной модели тестового пользователя - 1
+        testUserExtended1 = Profile.objects.get(user=testUser1)
+        testUserExtended1.bio = 'testUserBio'
+        self.assertEquals(testUserExtended1.bio, 'testUserBio')
+        
+        # выполняем проверку тестового пользователя - 2
+        testUser2 = User.objects.get(username='testUser2')
+        self.assertEquals(testUser2.username, 'testUser2')
+        self.assertEquals(testUser2.password, 'p@ssw0rd')
+        self.assertEquals(testUser2.email, 'testUser@gmail.com')
+        self.assertEquals(testUser2.first_name, 'userFirstName')
+        self.assertEquals(testUser2.last_name, 'userLastName')
+        # выполняем проверку расширенной модели тестового пользователя - 2
+        testUserExtended2 = Profile.objects.get(user=testUser2)
+        testUserExtended2.bio = 'testUserBio'
+        self.assertEquals(testUserExtended2.bio, 'testUserBio')
+
+        c = Client()
+        successLogin = c.login(username=testUser1.username, password=testUser1.password)
+        self.assertEquals(successLogin, True)
+        response = c.get(f'/post/{testPost.id}')
+        self.assertEquals(response.status_code, 200)
+        c.logout()
